b8f9de2848f1f07e38802ea976d3f7e18fe49429
sindi16 <10029@holbertonstudents.com>
2025-03-26 13:47:24 +0100

    changes

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return {
-    income,
-    gdp,
-    capita,
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
   };
+
+  return budget;
 }
