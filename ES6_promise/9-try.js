1df757181b0b4d8e3c85cab2396dabe11c7442ee
sindi16 <10029@holbertonstudents.com>
2025-04-04 12:31:30 +0200

    from 7 to 9

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction ){
+  const queue = [];
+  let res;
+
+  try{
+    res = mathFunction();
+  }catch(error){
+    res = error.toString();
+  }
+
+  queue.push(res);
+  queue.push("Guardrail was processed");
+  return queue;
+}
